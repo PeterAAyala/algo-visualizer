e512b03fc8b4b07b9c27ba81a254aebeaeb4fe7d
PeterAAyala <peteraayala@gmail.com>
2021-12-26 22:34:12 -0800

    Hold mouse button to fill
    Add ability to hold mouse button to fill multiple squares

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function Square(props) {
     <button
       className={props.class}
       onClick={props.onClick}
+      onMouseOver={props.onMouseOver}
+      onMouseDown={props.onMouseDown}
+      onMouseUp={props.onMouseUp}
     >
       {props.value}
     </button>
@@ -20,24 +23,52 @@ class Board extends React.Component {
       value: null,
       grid: Array(10).fill(null).map(x => Array(20).fill(null)),
       classGrid: Array(10).fill('square').map(x => Array(20).fill('square')),
+      mouseDown: false,
     };
   }
   
+  handleEvent = (event) => {
+    if (event.type === 'mousedown'){
+      this.setState({ mouseDown: true });
+      //console.log("test");
+    } else {
+      this.setState({ mouseDown: false });
+    }
+  }
+  
+
   renderSquare(i, j) {
     return (
       <Square 
         key = {i+j}
         class = {this.state.classGrid[i][j]}//'square'
         value = {this.state.grid[i][j]}
-        onClick = {() => this.handleClick(i,j)}/>
+        onClick = {() => this.handleClick(i,j)}
+        onMouseOver = {() => this.handleHover(i,j)}
+        onMouseDown = {this.handleEvent}
+        onMouseUp = {this.handleEvent}
+      />
     )
   };
+
+  handleHover (i, j){
+    if (this.state.mouseDown){
+      const current = this.state.grid.slice();
+      current[i][j] = 'X';
+      const classUpdate = this.state.classGrid.slice();
+      classUpdate[i][j] = 'square wall';
+      this.setState({
+        grid: current,
+        classGrid: classUpdate,
+      });
+    }
+  }
 
   handleClick (i, j) {
     const current = this.state.grid.slice();
     current[i][j] = 'X';
     const classUpdate = this.state.classGrid.slice();
-    classUpdate[i][j] = 'square wall'
+    classUpdate[i][j] = 'square wall';
     this.setState({
       grid: current,
       classGrid: classUpdate,
@@ -45,6 +76,7 @@ class Board extends React.Component {
   }
 
   render () {
+    //console.log(this.handleEvent);
     const GRID_ROW_LENGTH = 10;
     const GRID_COL_LENGTH = 20; 
     const grid = [];
@@ -71,46 +103,6 @@ class Board extends React.Component {
         })}
       </div>
     )};
-
-    /*
-    const items = [];
-    for (const [index, value] of this.state.squares.entries()) {
-      items.push({renderSquare(index)})
-    }
-
-    return (
-      <div>
-        {items}
-      </div>
-    )
-    
-    for (let i = 0; i < 3; i++){
-      <div className='board-row'>
-        for (let j = 0; j < 3; j++){}
-      </div>
-    }
-    
-
-
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    )};*/
 }
 
 
